d77f4fe85f68c902488d16fc8e523e35c905fbae
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-12-17 21:43:12 +0100

    Commit v1.0
    Commiting first version of my first console application created in NodeJS.

---
+++ README.md
@@ -0,0 +1,54 @@
+# Contacts Application
+
+The Contacts Application allows you to manage a list of contacts by adding, displaying, removing, and fetching contact information.
+
+## Features
+
+### Actions results:
+
+#### list
+
+**Displaying the Contact List**
+
+1. To display a list of all contacts, use the command:
+
+2. node index.js --action list
+
+![list](/images/list.png "List")
+
+#### get
+
+**Filtering Contacts**
+
+1. To fetch detailed information about a specific contact, use the command along with its ID:
+
+2. node index.js --action get --id ""
+
+![get](/images/get.png "Get")
+
+#### add
+
+**Adding a Contact**
+
+1. To add a new contact, use the command:
+
+2. node index.js --action add --name Jan --email jan@kowalski.com --phone 123-12-12
+
+![add](/images/add.png "Add")
+
+#### remove
+
+**Deleting a Contact**
+
+1. To remove a contact based on its ID, use the command:
+
+2. node index.js --action remove --id " "
+
+![remove](/images/remove.png "Remove")
+
+## Tech stack
+
+![JavaScript](https://badges.aleen42.com/src/javascript.svg)
+![NodeJS](https://badges.aleen42.com/src/node.svg)
+
+### Code editor: ![Code editor](https://badges.aleen42.com/src/visual_studio_code.svg)
---
+++ contacts.js
@@ -0,0 +1,62 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+const listContacts = async () => {
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    return JSON.parse(readContacts);
+  } catch (error) {
+    console.error("Load error", error.message);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const jsonContacts = await JSON.parse(readContacts);
+    return jsonContacts.filter((cont) => cont.id === contactId);
+  } catch (error) {
+    console.error("Get contact error", error.message);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const readContacts = await fs.readFile(contactsPath);    
+    const jsonContacts = await JSON.parse(readContacts);
+    const contact = jsonContacts.filter((cont) => cont.id === contactId);
+    const removedContact = await jsonContacts.filter((cont) => cont.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(removedContact));
+    return contact;
+  } catch (error) {
+    console.error("Remove contact error", error.message);
+  }
+};
+
+const addContact = async(name, email, phone) => {
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const jsonContacts = await JSON.parse(readContacts);
+    const addNewContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone
+    }
+    const newContacts = [...jsonContacts, addNewContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    return addNewContact;
+  } catch (error) {
+    console.error("Add contact error", error.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
